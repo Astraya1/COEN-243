978fd397aeb5a70ead16fb8aca0b373e45386468
Jma4679 <53155147+Jma4679@users.noreply.github.com>
2022-03-08 00:14:06 -0500

    Fixed bugs + added comments

--- Labs/Lab4.cpp
+++ Labs/Lab4.cpp
@@ -9,11 +9,12 @@ int main()
     void swapminmax(int (&arr)[5], int min, int max);
 
     int arr[5];
-    
-    cout << "Please enter 5 numbers for your array:" << endl;
 
     for (int i=0; i<5; i++)
+    {
+        cout << "Please enter number #" << i+1 << ": ";
         cin >> arr[i];
+    }
 
     cout << "Here is your original array: ";
 
@@ -32,27 +33,27 @@ int main()
         cout << arr[i] << " ";
 }
 
-int maxfunc(int arr[5])
+int maxfunc(int arr[5]) //Function that returns the index of the highest value in the array
 {
-    int currentmax = arr[0];
+    int currentmax = arr[0]; //Variable to keep track of the current highest value in the array while iterating
 
     for (int i=1; i<5; i++)
     {
-        if (arr[i] > arr[i-1])
-            currentmax = arr[i];
+        if (arr[i] > arr[i-1]) //Sets the currentmax variable to the index of the current value if it is greater than the previous value
+            currentmax = i;
     }
 
     return currentmax;
 }
 
-int minfunc(int arr[5])
+int minfunc(int arr[5]) //Same logic as maximum, except compares less rather than greater
 {
     int currentmin = arr[0];
 
     for (int i=1; i<5; i++)
     {
         if (arr[i] < arr[i-1])
-            currentmin = arr[i];
+            currentmin = i;
     }
 
     return currentmin;

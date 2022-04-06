28faa59757ac3585f9a6ffb1f3aee33841a8f88b
Jma4679 <53155147+Jma4679@users.noreply.github.com>
2022-04-06 19:27:44 -0400

    asdf

---
+++ Assignments/Group Project/student_staff.h
@@ -0,0 +1,30 @@
+#ifndef STUDENT_STAFF_H
+#define STUDENT_STAFF_H
+
+#include <iostream>
+#include <string>
+#include "students.h"
+#include "staff.h"
+using namespace std;
+
+class Student_Staff
+{
+private:
+    Students *ptrstudents;
+    Staff *ptrstaff;
+
+public:
+    //Constructor
+    Student_Staff();
+
+    //Getter functions
+    Students* getstudents();
+    Staff* getstaff();
+
+    //Member funcs
+    void highest_gpa(Students*);
+    int n_undergrad(Students*);
+    void same_hire_year(Staff*);
+};
+
+#endif
